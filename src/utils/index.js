3682cd5df7259553ea115de63aa2de288d3468a5
Jeffchiucp <jeffchiu2000@gmail.com>
2018-10-24 08:40:53 -0500

    utils uuid fixed

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,8 +2,10 @@ export function uuid(type) {
   var text
   if (type === 'container')
     text = "c"
-  else
+  else if (type === "item")
     text = "i"
+  else
+    return '' // throw not handled
   var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
   for (var i = 0; i < 8; i++)
