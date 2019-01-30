3d6623bd8dfeddf4645e5afc5e753036408c4898
Jeff Chiu <jeffchiu2000@gmail.com>
2019-01-30 09:44:46 -0800

    clean up the files and routes for arrangement data

--- src/services/servicetypes.js
+++ src/services/servicetypes.js
@@ -1,11 +1,10 @@
 export const base_url = () => {
-  if (process.env.NODE_ENV !== 'production') {
-    return process.env.REACT_APP_DEV_API_HOST
-  } else {
-    return process.env.REACT_APP_PROD_API_HOST
-  }
+    if (process.env.NODE_ENV !== 'production') {
+        return process.env.REACT_APP_DEV_API_HOST
+    } else {
+        return process.env.REACT_APP_PROD_API_HOST
+    }
 }
 
-// export const ARRANGEMENT = '/arrangement/aJDUX35L6'
 export const ARRANGEMENT = '/arrangement'
 export const EXPORT_ARRANGEMENT = '/arrangement'
