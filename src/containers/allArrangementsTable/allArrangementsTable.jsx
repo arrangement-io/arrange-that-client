57e66a9f80f23a3d3ac1bcd94575ffcef169fa15
atkwok <atkwok@berkeley.edu>
2019-02-26 22:07:01 -0800

    changed things to warn

--- src/containers/allArrangementsTable/allArrangementsTable.jsx
+++ src/containers/allArrangementsTable/allArrangementsTable.jsx
@@ -2,20 +2,19 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router'
 
-import PropTypes from 'prop-types';
-import { Link, Table, TableHead, TableRow, TableCell, TableBody } from '@material-ui/core';
+import { Table, TableHead, TableRow, TableCell, TableBody } from '@material-ui/core';
 
 class AllArrangementsTable extends Component {
     handleCellClick (id) {
         return () => {
-            console.log(id)
-            console.log(this.props)
+            // console.log(id)
+            // console.log(this.props)
             this.props.history.push('/arrangement/' + id)
         }
     }
 
     render () {
-        console.log(this.props.arrangements)
+        // console.log(this.props.arrangements)
         return (
             <Table>
                 <TableHead>
