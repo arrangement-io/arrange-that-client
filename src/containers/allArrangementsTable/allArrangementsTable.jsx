63a3dc7a2606bd766ac07555f2b0d7d7a6d8aaad
Gideon Chia <gchia@gchia-mn2.linkedin.biz>
2019-03-08 19:47:41 -0800

    You can now delete arrangements from the view all arrangements view

--- src/containers/allArrangementsTable/allArrangementsTable.jsx
+++ src/containers/allArrangementsTable/allArrangementsTable.jsx
@@ -3,18 +3,42 @@ import { connect } from 'react-redux'
 import { withRouter } from 'react-router'
 
 import { Table, TableHead, TableRow, TableCell, TableBody } from '@material-ui/core';
+import DeleteIcon from '@material-ui/icons/Delete';
+
+import { updateArrangement } from 'services/arrangementService'
+import { setArrangements } from 'actions/arrangements/arrangements'
 
 class AllArrangementsTable extends Component {
+    constructor(props) {
+        super(props);
+   }
+
     handleCellClick (id) {
         return () => {
-            // console.log(id)
-            // console.log(this.props)
             this.props.history.push('/arrangement/' + id)
         }
     }
 
+    // Figure out why it doesn't rerender
+    handleDeleteArrangement = (arrangement) => {
+        return () => {
+            const deletedArrangement = {
+                ...arrangement,
+                is_deleted: true
+            }
+            updateArrangement(deletedArrangement)
+                .then(response => {
+                    console.log("deleted arrangement")
+                    this.props.setArrangements(this.props.arrangements.filter(a => a._id !== deletedArrangement._id))
+                    Promise.resolve()
+                })
+                .catch(err => {
+                    Promise.reject(err)
+                })
+        }
+    }
+
     render () {
-        // console.log(this.props.arrangements)
         return (
             <Table>
                 <TableHead>
@@ -22,16 +46,18 @@ class AllArrangementsTable extends Component {
                         <TableCell>Arrangement Name</TableCell>
                         <TableCell align="right">Owner</TableCell>
                         <TableCell align="right">Last Modified</TableCell>
+                        <TableCell align="right"></TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
-                    {this.props.arrangements.map(row => (
-                        <TableRow hover={true} onClick={this.handleCellClick(row._id)} key={row._id}>
-                            <TableCell component="th" scope="row">
+                    {this.props.arrangements.filter(row => !row.is_deleted).map(row => (
+                        <TableRow hover={true} key={row._id}>
+                            <TableCell component="th" scope="row" onClick={this.handleCellClick(row._id)} >
                                 {row.name}
                             </TableCell>
                             <TableCell align="right">{row.owner}</TableCell>
                             <TableCell align="right">{row.modified_timestamp}</TableCell>
+                            <TableCell align="right"><DeleteIcon onClick={this.handleDeleteArrangement(row)}/></TableCell>
                         </TableRow>
                     ))}
                 </TableBody>
@@ -51,6 +77,9 @@ const mapStateToProps = (state, ownProps) => {
   
 const mapDispatchToProps = (dispatch, ownProps) => {
     return {
+        setArrangements: (arrangements) => {
+            dispatch(setArrangements(arrangements))
+        }
     }
 }
   
