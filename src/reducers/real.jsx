1dc2fbad14eaac0ff733b21ef87ae121df33a206
Gideon Chia <gchia@gchia-mn2.linkedin.biz>
2019-03-20 15:24:55 -0700

    indentation for real.jsx

--- src/reducers/real.jsx
+++ src/reducers/real.jsx
@@ -50,187 +50,187 @@ function exportState (real) {
 
 const realReducer = (state = initialState, action) => {
     switch (action.type) {
-    case ITEM_ADD: {
+        case ITEM_ADD: {
         // Add item to global item list
-        const addItemState = {
-            ...state,
-            items: [
-                ...state.items,
-                action.item,
-            ]
-        }
+            const addItemState = {
+                ...state,
+                items: [
+                    ...state.items,
+                    action.item,
+                ]
+            }
 
-        // Add item to all snapshot unassigned
-        for (let snapshot of addItemState.snapshots) {
-            snapshot.unassigned.push(action.item._id)
+            // Add item to all snapshot unassigned
+            for (let snapshot of addItemState.snapshots) {
+                snapshot.unassigned.push(action.item._id)
+            }
+            exportState(addItemState)
+            return addItemState
         }
-        exportState(addItemState)
-        return addItemState
-    }
                 
-    case ITEM_DELETE: {
-        const deleteItemState = {
-            ...state
-        }
+        case ITEM_DELETE: {
+            const deleteItemState = {
+                ...state
+            }
 
-        // Remove item from global items list
-        deleteItemState.items = deleteItemState.items.filter(ele => ele._id !== action.id)
+            // Remove item from global items list
+            deleteItemState.items = deleteItemState.items.filter(ele => ele._id !== action.id)
 
-        // Remove item from all snapshots
-        for (let snapshot of deleteItemState.snapshots) {
+            // Remove item from all snapshots
+            for (let snapshot of deleteItemState.snapshots) {
             // Remove item from all containers in snapshot
-            for (let container of snapshot.snapshotContainers) {
-                container = container.items.filter(ele => ele !== action.id)
+                for (let container of snapshot.snapshotContainers) {
+                    container = container.items.filter(ele => ele !== action.id)
+                }
+                // Remove item from unassigned in snapshot
+                snapshot.unassigned = snapshot.unassigned.filter(ele => ele !== action.id)
             }
-            // Remove item from unassigned in snapshot
-            snapshot.unassigned = snapshot.unassigned.filter(ele => ele !== action.id)
+            exportState(deleteItemState)
+            return deleteItemState
         }
-        exportState(deleteItemState)
-        return deleteItemState
-    }
 
-    case ITEM_RENAME: {
-        let items = state.items
-        let item = items.find(ele => ele._id === action.item._id)
-        item.name = action.item.name
-        items = items.filter(ele => ele._id !== action.item._id)
-        const resultItemRename = {
-            ...state,
-            items: [
-                ...items,
-                item
-            ]
+        case ITEM_RENAME: {
+            let items = state.items
+            let item = items.find(ele => ele._id === action.item._id)
+            item.name = action.item.name
+            items = items.filter(ele => ele._id !== action.item._id)
+            const resultItemRename = {
+                ...state,
+                items: [
+                    ...items,
+                    item
+                ]
+            }
+            exportState(resultItemRename)
+            return resultItemRename
         }
-        exportState(resultItemRename)
-        return resultItemRename
-    }
 
-    case CONTAINER_ADD: {
+        case CONTAINER_ADD: {
         // Add container to global
-        const addContainerState = {
-            ...state,
-            containers: [
-                ...state.containers,
-                action.container
-            ]
-        }
-        // Add container to all snapshots
-        for (let snapshot of addContainerState.snapshots) {
-            snapshot.snapshotContainers.push({_id: action.container._id, items: []})
-        }
-
-        exportState(addContainerState)
-        return addContainerState
-    }
-
-    case CONTAINER_DELETE: {
-        const deleteContainerState = {
-            ...state
-        }
-        // Remove container from global
-        deleteContainerState.containers = deleteContainerState.containers.filter(ele => ele._id !== action.id)
-
-        // Delete container to all snapshots
-        for (let snapshot of deleteContainerState.snapshots) {
-            const containerIndex = getSnapshotContainerIndex(snapshot, action.id)
-            for (let item of snapshot.snapshotContainers[containerIndex].items) {
-                snapshot.unassigned.push(item)
+            const addContainerState = {
+                ...state,
+                containers: [
+                    ...state.containers,
+                    action.container
+                ]
             }
-            snapshot.snapshotContainers.splice(containerIndex, 1)
+            // Add container to all snapshots
+            for (let snapshot of addContainerState.snapshots) {
+                snapshot.snapshotContainers.push({_id: action.container._id, items: []})
+            }
+
+            exportState(addContainerState)
+            return addContainerState
         }
-        exportState(deleteContainerState)
-        return deleteContainerState
-    }
 
-    case CONTAINER_EDIT: {
-        const container = state.containers.find(ele => ele && ele._id === action.container._id)
-        container.name = action.container.name
-        container.size = action.container.size
+        case CONTAINER_DELETE: {
+            const deleteContainerState = {
+                ...state
+            }
+            // Remove container from global
+            deleteContainerState.containers = deleteContainerState.containers.filter(ele => ele._id !== action.id)
 
-        const resultContainerRename = {
-            ...state
+            // Delete container to all snapshots
+            for (let snapshot of deleteContainerState.snapshots) {
+                const containerIndex = getSnapshotContainerIndex(snapshot, action.id)
+                for (let item of snapshot.snapshotContainers[containerIndex].items) {
+                    snapshot.unassigned.push(item)
+                }
+                snapshot.snapshotContainers.splice(containerIndex, 1)
+            }
+            exportState(deleteContainerState)
+            return deleteContainerState
         }
-        exportState(resultContainerRename)
-        return resultContainerRename
-    }
 
-    case SET_REAL_DATA: {
-        exportState(action.data)
-        return action.data
-    }
+        case CONTAINER_EDIT: {
+            const container = state.containers.find(ele => ele && ele._id === action.container._id)
+            container.name = action.container.name
+            container.size = action.container.size
+
+            const resultContainerRename = {
+                ...state
+            }
+            exportState(resultContainerRename)
+            return resultContainerRename
+        }
+
+        case SET_REAL_DATA: {
+            exportState(action.data)
+            return action.data
+        }
         
 
-    case SET_UNASSIGNED_ITEMS: {
-        const setUnassignedState = {
-            ...state
-        }
-        const index = getSnapshotIndex(setUnassignedState, action.snapshotId)
-        setUnassignedState.snapshots[index].unassigned = action.unassigned
+        case SET_UNASSIGNED_ITEMS: {
+            const setUnassignedState = {
+                ...state
+            }
+            const index = getSnapshotIndex(setUnassignedState, action.snapshotId)
+            setUnassignedState.snapshots[index].unassigned = action.unassigned
             
-        exportState(setUnassignedState)
-        return setUnassignedState
-    }
-
-    case SET_CONTAINER_ITEMS: {
-        const setContainerItemsState = {
-            ...state
+            exportState(setUnassignedState)
+            return setUnassignedState
         }
-        const index = getSnapshotIndex(setContainerItemsState, action.snapshotId)
-        const containerIndex = getSnapshotContainerIndex(setContainerItemsState.snapshots[index], action.containerId)
-        setContainerItemsState.snapshots[index].snapshotContainers[containerIndex].items = action.items
 
-        exportState(setContainerItemsState)
-        return setContainerItemsState
-    }
+        case SET_CONTAINER_ITEMS: {
+            const setContainerItemsState = {
+                ...state
+            }
+            const index = getSnapshotIndex(setContainerItemsState, action.snapshotId)
+            const containerIndex = getSnapshotContainerIndex(setContainerItemsState.snapshots[index], action.containerId)
+            setContainerItemsState.snapshots[index].snapshotContainers[containerIndex].items = action.items
 
-    case SAVE_STATE: {
-        exportState(state)
-        return state
-    }
+            exportState(setContainerItemsState)
+            return setContainerItemsState
+        }
+
+        case SAVE_STATE: {
+            exportState(state)
+            return state
+        }
             
-    case SNAPSHOT_ADD: {
-        const snapshotAddState = {
-            ...state
-        }
-        snapshotAddState.snapshots.push(action.snapshot)
-        exportState(snapshotAddState)
-        return snapshotAddState
-    }
-
-    case SNAPSHOT_DELETE: {
-        const snapshotDeleteState = {
-            ...state
+        case SNAPSHOT_ADD: {
+            const snapshotAddState = {
+                ...state
+            }
+            snapshotAddState.snapshots.push(action.snapshot)
+            exportState(snapshotAddState)
+            return snapshotAddState
         }
 
-        const index = getSnapshotIndex(snapshotDeleteState, action.snapshotId)
-        snapshotDeleteState.snapshots.splice(index, 1)
-        exportState(snapshotDeleteState)
-        return snapshotDeleteState
-    }
+        case SNAPSHOT_DELETE: {
+            const snapshotDeleteState = {
+                ...state
+            }
 
-    case ARRANGEMENT_RENAME:
-        const arrangementRenameState = {
-            ...state,
-            name: action.name
-        }
-        exportState(arrangementRenameState)
-        return arrangementRenameState
-
-
-    case SNAPSHOT_RENAME: {
-        const snapshotRenameState = {
-            ...state
+            const index = getSnapshotIndex(snapshotDeleteState, action.snapshotId)
+            snapshotDeleteState.snapshots.splice(index, 1)
+            exportState(snapshotDeleteState)
+            return snapshotDeleteState
         }
 
-        const index = getSnapshotIndex(snapshotRenameState, action.snapshotId)
-        snapshotRenameState.snapshots[index].name = action.name
-        exportState(snapshotRenameState)
-        return snapshotRenameState
-    }
+        case ARRANGEMENT_RENAME:
+            const arrangementRenameState = {
+                ...state,
+                name: action.name
+            }
+            exportState(arrangementRenameState)
+            return arrangementRenameState
 
-    default: {
-        return state
-    }
+
+        case SNAPSHOT_RENAME: {
+            const snapshotRenameState = {
+                ...state
+            }
+
+            const index = getSnapshotIndex(snapshotRenameState, action.snapshotId)
+            snapshotRenameState.snapshots[index].name = action.name
+            exportState(snapshotRenameState)
+            return snapshotRenameState
+        }
+
+        default: {
+            return state
+        }
     }
 }
 
