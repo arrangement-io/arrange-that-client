8446ec463e8dd012371899f13ee51081ac4fd976
Gideon Chia <gchia@gchia-mn2.linkedin.biz>
2019-03-21 13:57:27 -0700

    added rename and delete to snapshot

---
+++ src/components/snapshotTitle/snapshotTitle.jsx
@@ -0,0 +1,124 @@
+import React, { Component } from 'react'
+
+import MoreMenu from 'components/moremenu/moremenu'
+
+import PropTypes from 'prop-types'
+import { Typography } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles'
+
+import { TextField } from '@material-ui/core'
+
+const ENTER_KEY = 13;
+const ESC_KEY = 27;
+
+const styles = theme => ({
+    root: {
+        display: "flex",
+        justifyContent: "space-between"
+    },
+    snapshotTitle: {
+        flexGrow: 1,
+        textOverflow: "ellipsis",
+        overflow: "hidden"
+    },
+    snapshotMenu: {
+        position: "relative",
+        bottom: "12px"
+    },
+    editSnapshotTitle: {
+        position: "relative",
+        bottom: "12px"
+    }
+})
+
+class SnapshotTitle extends Component {
+    constructor (props) {
+        super(props)
+
+        this.state = {
+            isEdit: false,
+            newName: props.snapshot.name
+        }
+    }
+
+    handleItemClick = (option) => {
+        if (option === 'Delete') {
+            this.props.onDelete(this.props.snapshot._id)
+        }
+        else if (option === 'Edit') {
+            this.setState({
+                ...this.state,
+                isEdit: true
+            })
+        }
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            ...this.state,
+            newName: e.target.value
+        })
+    }
+
+    handleKeyDown = (e) => {
+        switch (e.keyCode) {
+            case ENTER_KEY:
+                this.handleEnter(this.state.newName)
+                break;
+            case ESC_KEY:
+                this.setState({
+                    ...this.state,
+                    isEdit: false,
+                    newName: this.props.snapshot.name
+                })
+            default:
+                break;
+        }    
+    }
+
+    handleEnter = (name) => {
+        this.props.onSave(this.props.snapshot._id, name)
+        this.setState({
+            ...this.state,
+            isEdit: false
+        })
+    }
+
+    render = () => {
+        const { classes } = this.props
+        const options = [
+            'Edit',
+            'Delete'
+        ]
+
+        const title = (
+            <div className={classes.root}>
+                <Typography className={classes.snapshotTitle} variant="body1">{this.props.snapshot.name}</Typography>
+                <div className={classes.snapshotMenu}>
+                    <MoreMenu options = {options} handleItemClick = {this.handleItemClick} />
+                </div>
+            </div>
+        )
+
+        const editTitle = (
+            <div className={classes.editSnapshotTitle}>
+                <TextField
+                    autoFocus={true}
+                    defaultValue={this.props.snapshot.name}
+                    onKeyDown={this.handleKeyDown}
+                    onChange={this.handleChange}
+                    onBlur={() => this.handleEnter(this.state.newName)}
+                    margin="dense"
+                />
+            </div>
+        )
+
+        return this.state.isEdit ? editTitle : title;
+    }
+}
+
+SnapshotTitle.propTypes = {
+    snapshot: PropTypes.object
+}
+
+export default withStyles(styles)(SnapshotTitle)
--- src/views/arrangement/arrangement.jsx
+++ src/views/arrangement/arrangement.jsx
@@ -6,11 +6,12 @@ import { Grid, Typography } from '@material-ui/core'
 import Snapshot from 'containers/snapshot/snapshot'
 
 import ExportButton from 'components/exportbutton/exportbutton'
+import SnapshotTitle from 'components/snapshotTitle/snapshotTitle'
 
 import { getArrangement } from 'services/arrangementService'
 
 import { setRealData, arrangementRename } from 'actions/real/real'
-import { snapshotAdd } from 'actions/snapshot/snapshot'
+import { snapshotAdd, snapshotDelete, snapshotRename } from 'actions/snapshot/snapshot'
 import { uuid } from 'utils'
 
 import Tabs, { Tab } from 'react-awesome-tabs';
@@ -31,13 +32,24 @@ export class Arrange extends Component {
         this.setState({activeTab: active})
     }
 
-    handleTabAdd() {
+    handleTabAdd = () => {
         const numberOfCurrentSnapshots = this.props.real.snapshots.length
         this.props.snapshotAdd(this.createNewSnapshot())
 
         this.setState({
             activeTab: numberOfCurrentSnapshots
         });
+    }
+    
+    deleteSnapshot = (snapshotId) => {
+        this.props.snapshotDelete(snapshotId)
+        const numberOfCurrentSnapshots = this.props.real.snapshots.length
+        console.log(numberOfCurrentSnapshots)
+        this.setState({
+            ...this.state,
+            activeTab: numberOfCurrentSnapshots-1,
+        });
+        console.log(this.state)
     }
 
     handleArrangementTitleEnter = (name) => {
@@ -129,6 +141,7 @@ export class Arrange extends Component {
     }
     
     render () {
+        console.log(this.state)
         return (
             <div>
                 <Grid container spacing={8} className="arrange">
@@ -147,13 +160,17 @@ export class Arrange extends Component {
                 </Grid>
                 <Tabs 
                     active={this.state.activeTab} 
-                    onTabSwitch={this.handleTabSwitch.bind(this)}
-                    onTabAdd={this.handleTabAdd.bind(this)}
+                    onTabSwitch={this.handleTabSwitch}
+                    onTabAdd={this.handleTabAdd}
                     showAdd={true}
                 >
                     {this.props.real.snapshots.map((snapshot, index) => {
                         return (
-                            <Tab key={index} title={snapshot.name}>
+                            <Tab key={index} title={
+                                <SnapshotTitle 
+                                    snapshot={snapshot} 
+                                    onDelete={this.deleteSnapshot} 
+                                    onSave={this.props.snapshotRename} /> }>
                                 <Snapshot snapshotId={snapshot._id} />
                             </Tab>
                         )
@@ -179,6 +196,12 @@ const mapDispatchToProps = (dispatch, ownProps) => {
         },
         snapshotAdd: (snapshot) => {
             dispatch(snapshotAdd(snapshot))
+        },
+        snapshotDelete: (snapshotId) => {
+            dispatch(snapshotDelete(snapshotId))
+        },
+        snapshotRename: (snapshotId, name) => {
+            dispatch(snapshotRename(snapshotId, name))
         }
     }
 }
