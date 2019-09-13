f095a46f4029b77e4c5dfc169fe3d68be727bf83
Gideon Chia <gchia@gchia-mn2.linkedin.biz>
2019-09-13 09:15:31 -0700

    Added a main and set the theme to blue

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,35 @@
 import React, { Component } from 'react'
 import { Provider } from 'react-redux'
+import { SnackbarProvider } from 'notistack';
+import { MuiThemeProvider } from '@material-ui/core/styles';
+import axios from 'axios';
+
 import router from 'config/router'
 import store from 'store/store'
-import { SnackbarProvider } from 'notistack';
-import axios from 'axios';
 import { logout } from 'actions/account/account'
-import 'handsontable/dist/handsontable.full.css';
+import mainTheme from 'theme/mainTheme'
 
+import 'handsontable/dist/handsontable.full.css';
 import './App.css'
 
 class App extends Component {
     render() {
         return (
-            <SnackbarProvider 
-                maxSnack={3} preventDuplicate 
-                anchorOrigin={{
-                    vertical: 'bottom',
-                    horizontal: 'right',
-                }}
-            >
-                <Provider store={store}>
-                    <div className="App">
-                        { router }
-                    </div>
-                </Provider>
-            </SnackbarProvider>
+            <MuiThemeProvider theme={mainTheme}>
+                <SnackbarProvider 
+                    maxSnack={3} preventDuplicate 
+                    anchorOrigin={{
+                        vertical: 'bottom',
+                        horizontal: 'right',
+                    }}
+                >
+                    <Provider store={store}>
+                        <div className="App">
+                            { router }
+                        </div>
+                    </Provider>
+                </SnackbarProvider>
+            </MuiThemeProvider>
         )
     }
 }
